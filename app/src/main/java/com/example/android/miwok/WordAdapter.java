aaaea1ce6fd5a5245133afc2cdd369989dc9f2c8
JS IID <haseebb.kithran@gmail.com>
2017-09-18 19:50:12 +0500

    yaay

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,89 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by JS IID on 6/9/2017.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    private int mColorResourceId;
+
+
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context The current context. Used to inflate the layout file.
+     * @param words   A List of word objects to display in a list
+     */
+    public WordAdapter(Activity context, ArrayList<Word> words,int colorResourceId) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
+    }
+
+    @NonNull
+    @Override
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView MiwokTextView = (TextView) listItemView.findViewById(R.id.miw1);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        MiwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView EnglishTextView = (TextView) listItemView.findViewById(R.id.eng1);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        EnglishTextView.setText(currentWord.getDefaultTranslation());
+
+
+
+        ImageView Image1View = (ImageView) listItemView.findViewById(R.id.img1);
+
+        if (currentWord.hasImage()) {
+            Image1View.setImageResource(currentWord.getImageResourceId());
+            Image1View.setVisibility(View.VISIBLE);
+
+        } else {
+            Image1View.setVisibility(View.GONE);
+        }
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+}
